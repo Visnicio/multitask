17242d7e8f1a8fbea22f7fe205bdd8babbb9a6a8
Visnicio <vinicius.antonio0712@gmail.com>
2025-06-02 22:24:02 -0300

    login automatico

--- frontend/app/controllers/registrationController.js
+++ frontend/app/controllers/registrationController.js
@@ -20,7 +20,27 @@ angular.module('multitask-front')
                     return;
                 }
 
-                $location.path('/login');
+                authService.attemptLogin($scope.user_email, $scope.user_password).then(function(response) {
+                if (response.data.error) {
+                    alert(response.data.error);
+                    return;
+                }
+
+                let token = response.data.access_token;
+                localStorage.setItem('token', token);
+                
+                // save current user
+                authService.requestUserData().then(function(response) {
+                    if (response.data.status == "error") {
+                        alert(response.data.error);
+                        return;
+                    }
+                    authService.setUser(response.data);
+
+                    $rootScope.$broadcast('userChanged');
+                    $location.path('/dashboard');
+                })
+            })
             });
         };
     });
